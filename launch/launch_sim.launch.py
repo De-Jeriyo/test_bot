9806b42e6787a9a08b6ce08a324e3244b4f7d674
De-Jeriyo <derekjeriyo@gmail.com>
2025-01-23 11:06:23 +0630

    Added Nav2

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -24,6 +24,15 @@ def generate_launch_description():
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items() #
     )
+    
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
@@ -58,7 +67,8 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
+        rsp,        
+        twist_mux,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
